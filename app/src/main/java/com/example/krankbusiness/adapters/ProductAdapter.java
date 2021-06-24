596541418c366d8d191a2edbfdddcc67f5d41b51
monjur35 <monjur35-1019@diu.edu.bd>
2021-06-24 23:54:56 +0600

    view Added Product

---
+++ app/src/main/java/com/example/krankbusiness/adapters/ProductAdapter.java
@@ -0,0 +1,73 @@
+package com.example.krankbusiness.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.krankbusiness.R;
+import com.example.krankbusiness.models.Product;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
+
+    private List<Product>productList;
+    private Context context;
+
+    public ProductAdapter(List<Product> productList, Context context) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View item= LayoutInflater.from(context).inflate(R.layout.product_row,parent,false);
+        return new ProductViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ProductAdapter.ProductViewHolder holder, int position) {
+
+        Product product=productList.get(position);
+        holder.productName.setText(product.getProductName());
+        holder.price.setText(product.getProductPrice()+" BDT");
+        holder.inStock.setText(product.getTotalInStock()+" Pcs");
+        if (product.getTotalInStock()==0){
+            holder.card.setBackgroundColor(Color.parseColor("#FF8585"));
+        }
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView productName,price,inStock;
+        LinearLayout card;
+
+        public ProductViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            productName=itemView.findViewById(R.id.productName);
+            price=itemView.findViewById(R.id.price);
+            inStock=itemView.findViewById(R.id.inStock);
+            card=itemView.findViewById(R.id.cardView);
+        }
+    }
+}
--- app/src/main/java/com/example/krankbusiness/fragments/AddProducrFragment.java
+++ app/src/main/java/com/example/krankbusiness/fragments/AddProducrFragment.java
@@ -8,12 +8,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.krankbusiness.R;
+import com.example.krankbusiness.adapters.ProductAdapter;
 import com.example.krankbusiness.databinding.FragmentAddProducrBinding;
 import com.example.krankbusiness.models.Product;
 import com.example.krankbusiness.viewModels.KrankViewModel;
@@ -21,12 +24,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class AddProducrFragment extends Fragment {
     private FragmentAddProducrBinding binding;
     private KrankViewModel krankViewModel;
+
+    private ProductAdapter productAdapter;
+    private LinearLayoutManager linearLayoutManager;
+    private List<Product>productList;
 
     private BottomNavigationView bottomNavigationView;
 
@@ -43,6 +51,11 @@ public class AddProducrFragment extends Fragment {
         krankViewModel=new ViewModelProvider(getActivity()).get(KrankViewModel.class);
         bottomNavigationView=getActivity().findViewById(R.id.bottom_navigation);
         bottomNavigationView.setVisibility(View.VISIBLE);
+
+        productList=new ArrayList<>();
+        productAdapter=new ProductAdapter(productList,getContext());
+        linearLayoutManager=new LinearLayoutManager(getActivity());
+
         return binding.getRoot();
     }
 
@@ -50,13 +63,24 @@ public class AddProducrFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.spinKit.setVisibility(View.VISIBLE);
+
         krankViewModel.getAllProductList().observe(getViewLifecycleOwner(), new Observer<List<Product>>() {
             @Override
             public void onChanged(List<Product> products) {
                 if (products!=null){
 
+                    productList.addAll(products);
+                    binding.productRv.setAdapter(productAdapter);
+                    binding.productRv.setLayoutManager(linearLayoutManager);
+
+                    binding.spinKit.setVisibility(View.INVISIBLE);
 
                 }
+                else {
+                    Toast.makeText(getContext(), "No data found", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
